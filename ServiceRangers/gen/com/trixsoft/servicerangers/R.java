917bee3fbe62e82d88458a6caaac1ea42bb370ef
satishkunchala <satishkunchala@gmail.com>
2014-10-18 11:33:15 +0530

    First Cut of code
    First cut of code with icons

--- ServiceRangers/gen/com/trixsoft/servicerangers/R.java
+++ ServiceRangers/gen/com/trixsoft/servicerangers/R.java
@@ -1077,9 +1077,17 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int background=0x7f020057;
+        public static final int bg=0x7f020058;
+        public static final int button=0x7f020059;
+        public static final int ic_checkin=0x7f02005a;
+        public static final int ic_checkout=0x7f02005b;
+        public static final int ic_issues=0x7f02005c;
+        public static final int ic_launcher=0x7f02005d;
+        public static final int ic_reports=0x7f02005e;
     }
     public static final class id {
+        public static final int RelativeLayout1=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,12 +1099,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
-        public static final int button2=0x7f05003f;
+        public static final int button1=0x7f05003f;
+        public static final int button2=0x7f050042;
         public static final int button3=0x7f050040;
         public static final int button4=0x7f050041;
         public static final int checkbox=0x7f05002d;
@@ -1141,7 +1149,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
+        public static final int textView1=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1198,9 +1206,10 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_checkin=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int fragment_main=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1250,8 +1259,10 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int checkin=0x7f0a0010;
         public static final int checkout=0x7f0a0011;
+        public static final int hello_world=0x7f0a0015;
         public static final int issues=0x7f0a0012;
         public static final int reports=0x7f0a0013;
+        public static final int title_activity_checkin=0x7f0a0014;
         public static final int welcome=0x7f0a000e;
     }
     public static final class style {
